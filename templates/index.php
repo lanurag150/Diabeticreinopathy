04b48e1daee0fb7ed0aae7aee0a0b1265a8be6b8
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-30 14:53:07 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -82,7 +82,7 @@
       <div class="container col-lg-6" >
           
           
-           <div class="w3-card-4" style="width: 700px; height:550px; padding:20px;background-color:AntiqueWhite">
+           <div class="w3-card-4" style="width: 700px; height:550px; padding:20px;background-color:White">
               <div class="w3-container w3-black">
                 <h2 style="color:white;">Diabetic Reinopathy Analysis</h2>
               </div>
